afb1a4e163cc6cfafef622c4b7fa7ed5682adbce
tomas richard <richardtomas92@gmail.com>
2023-09-17 15:38:49 -0300

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Tomas Richard on 17/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
